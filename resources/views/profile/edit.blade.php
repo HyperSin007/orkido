445c788599ca7e57ba5ede8e417d9524e7d0ab8c
HyperSin007 <shuvrosikder99@gmail.com>
2025-06-27 04:10:20 +0300

    Update profile edit view to match admin panel design

--- resources/views/profile/edit.blade.php
+++ resources/views/profile/edit.blade.php
@@ -1,29 +1,18 @@
-<x-app-layout>
-    <x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            {{ __('Profile') }}
-        </h2>
-    </x-slot>
+@extends('layouts.admin')
 
-    <div class="py-12">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8 space-y-6">
-            <div class="p-4 sm:p-8 bg-white shadow sm:rounded-lg">
-                <div class="max-w-xl">
-                    @include('profile.partials.update-profile-information-form')
-                </div>
-            </div>
-
-            <div class="p-4 sm:p-8 bg-white shadow sm:rounded-lg">
-                <div class="max-w-xl">
-                    @include('profile.partials.update-password-form')
-                </div>
-            </div>
-
-            <div class="p-4 sm:p-8 bg-white shadow sm:rounded-lg">
-                <div class="max-w-xl">
-                    @include('profile.partials.delete-user-form')
-                </div>
-            </div>
+@section('content')
+<div class="max-w-3xl mx-auto w-full p-4 lg:p-8">
+    <h2 class="text-2xl font-bold mb-8 text-center bg-white py-4 rounded shadow">Edit Profile</h2>
+    <div class="space-y-8">
+        <div class="bg-white shadow rounded p-6">
+            @include('profile.partials.update-profile-information-form')
+        </div>
+        <div class="bg-white shadow rounded p-6">
+            @include('profile.partials.update-password-form')
+        </div>
+        <div class="bg-white shadow rounded p-6">
+            @include('profile.partials.delete-user-form')
         </div>
     </div>
-</x-app-layout>
+</div>
+@endsection
